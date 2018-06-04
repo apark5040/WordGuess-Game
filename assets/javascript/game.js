a15e4151ec4211cf8fc2d337e5b94cac0d4087c6
Andrew Park <apark11278@gmail.com>
2018-06-04 18:14:32 -0400

    Changed starting page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,71 +2,112 @@ var stuffList = ['peanut', 'night', 'bed', 'pony', 'letter'];
 
 var hangmanGuess = stuffList[Math.floor(Math.random() * stuffList.length)];
 
+var game = {
+correctAns : [],
+incorrectAns :[],
+combineAnswer: '' ,
+wins : 0,
+misses : 0,
+firstDiv : $("<p>"),
+secondDiv : $("<p>")
+}
 
-var correctAns = [];
-var incorrectAns =[];
-var combineAnswer;
-var wins = 0;
-var misses = 0;
-var firstDiv = $("<p>");
-var secondDiv = $("<p>");
-firstDiv.addClass("hangmanWord");
-secondDiv.addClass("hangmanWord");
-$("#game").append(firstDiv);
-$("#game").append(secondDiv);
+
+var parent = document.getElementById("game");
+var child = document.getElementById("startMessage");
+    
+
+
+game.firstDiv.addClass("hangmanWord");
+game.secondDiv.addClass("hangmanWord");
+$("#game").append(game.firstDiv);
+$("#game").append(game.secondDiv);
+
+
 
 for(i = 0; i < hangmanGuess.length; i++){
-    correctAns.splice(i, 0, "_");
-    
+    game.correctAns.splice(i, 0, "_");  
 }
+
+var wordDisplay = game.correctAns.join(" ");
+
+$(game.firstDiv).text(wordDisplay);
+
+function reset(){
+  
+    game.correctAns = [];
+    game.incorrectAns =[];
+    game.combineAnswer = "";
+  
+    hangmanGuess = stuffList[Math.floor(Math.random() * stuffList.length)];
+  for(i = 0; i < hangmanGuess.length; i++){
+    game.correctAns.splice(i, 0, "_");  
+}
+
+  $(game.firstDiv).text(wordDisplay);
+  
+}
+
+
 
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key.toLowerCase();
-    
-
 
     console.log(hangmanGuess);
     
-    console.log(userGuess);
-
-    for (i = 0; i < hangmanGuess.length; i++) {
+    
+    
+    if(parent.contains(child) === true){
+      parent.removeChild(child);
+      
+    }
+     
+    
+      
+       
+      
+      for (i = 0; i < hangmanGuess.length; i++) {
         if(userGuess == hangmanGuess[i]){
-            correctAns[i] = userGuess;
-            combineAnswer = correctAns.join(" ");
-            if(correctAns.includes("_")){
-                combineAnswer = correctAns.join(" ");
-                
-            }
-            else{
-                //insert win quote here
+            game.correctAns[i] = userGuess;
+            game.combineAnswer = game.correctAns.join(" ");
+            if(game.correctAns.includes("_")){
+                game.combineAnswer = game.correctAns.join(" ");    
             }
         }
 
         else {
             
-            combineAnswer = correctAns.join(" ");
-            misses++;
-            
-            
-            
-
+            game.combineAnswer = game.correctAns.join(" ");
+           
         }
         
-        
-    }
-
-    if(correctAns.indexOf(userGuess) < 0){
-        incorrectAns.push(userGuess);
-    }
+      
     
     
+    
+ 
+    }
 
-    $(firstDiv).text(combineAnswer);
-    $(secondDiv).text(incorrectAns);
+    if(game.correctAns.indexOf(userGuess) < 0){
+        game.incorrectAns.push(userGuess);
+        game.misses++;
+      
+    }
+  
+    if(game.combineAnswer.replace(/\s/g, '') == hangmanGuess){
+      game.wins++;
+      reset();
+    }
+  
+    
+    
+    $(game.firstDiv).text(game.combineAnswer);
+    $(game.secondDiv).text(game.incorrectAns);
 
-
+    console.log("Misses: " + game.misses);
+    console.log("Wins: " + game.wins);
 
 
 
